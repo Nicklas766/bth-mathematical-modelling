5218291eee9fbd98d4f7ea728a8c9dcc3da06ac3
Nicklas766 <Nicklas766@live.se>
2017-12-12 13:53:39 +0100

    kmom02 completed

--- app.py
+++ app.py
@@ -9,30 +9,58 @@ My first Flask app
 from flask import Flask, render_template
 from person import Person
 
-APP = Flask(__name__)
+app = Flask(__name__)
 
 PERSON_OBJECT = Person("Nicklas", "Envall", "21")
 
 # Make it easier to debug
-APP.debug = True
-APP.config.update(
+app.debug = True
+app.config.update(
     PROPAGATE_EXCEPTIONS=True
 )
 
-@APP.route("/")
+@app.route("/")
 def main():
     """ Main route """
     return render_template("index.html")
 
-@APP.route("/about")
+@app.route("/about")
 def about():
     """ About route """
     return render_template("about.html", person=PERSON_OBJECT)
 
-@APP.route("/uppgift1")
+@app.route("/uppgift1")
 def uppgift1():
     """ uppgift1 route """
     return render_template("uppgift1.html")
 
+@app.route("/centrala")
+def centrala():
+    """ centrala route """
+    return render_template("centrala.html")
+
+@app.route("/montecarlo")
+def montecarlo():
+    """ centrala route """
+    return render_template("montecarlo.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """
+    Handler for page not found 404
+    """
+    #pylint: disable=unused-argument
+    return "Flask 404 here, but not the page you requested."
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    """
+    Handler for internal server error 500
+    """
+    #pylint: disable=unused-argument
+    import traceback
+    return "<p>Flask 500<pre>" + traceback.format_exc()
+
 if __name__ == "__main__":
-    APP.run()
+    app.run()
